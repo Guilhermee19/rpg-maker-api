1322b7bbf1085dfd07f3cf2d12edf8de3903e8b5
Guilherme Santana <guilherme.santana1998@gmail.com>
2026-02-26 15:29:13 -0300

    feat: Update ItemAdmin to use session_link for improved session display in admin

--- items/admin.py
+++ items/admin.py
@@ -4,7 +4,14 @@ from .models import Item
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'session', 'category', 'rarity', 'durability_current', 'durability_max')
+    list_display = ('name', 'session_link', 'category', 'rarity', 'durability_current', 'durability_max')
     list_filter = ('category', 'rarity', 'session')
-    search_fields = ('name', 'category', 'rarity', 'session__name')
-    raw_id_fields = ('session',)
+    search_fields = ('name', 'session__name', 'category', 'rarity', 'session__name')
+    # raw_id_fields = ('session',)
+
+    def session_link(self, obj):
+        if obj.session:
+            return f'<a href="/admin/session/session/{obj.session.id}/change/">{obj.session.name}</a>'
+        return "-"
+    session_link.allow_tags = True
+    session_link.short_description = 'Session'
