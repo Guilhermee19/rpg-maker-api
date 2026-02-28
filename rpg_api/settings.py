4f59b7faa137ac0cae789c743902d82e6c858ad3
Guilhermee19 <guilherme.santana1998@gmail.com>
2026-02-28 09:41:47 -0300

    fix: Update EMAIL_BACKEND to use SMTP for email sending

--- rpg_api/settings.py
+++ rpg_api/settings.py
@@ -224,7 +224,8 @@ CORS_ALLOW_HEADERS = [
 
 
 # Email (exemplo com Gmail, ajuste para seu provider)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
